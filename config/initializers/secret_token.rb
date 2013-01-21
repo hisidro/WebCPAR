cc8af198291cbfc56171a8a11f3c7a3c66689d21
Homer Isidro <homer.isidro@gmail.com>
2013-01-21 15:57:40 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebCPAR::Application.config.secret_token = 'd76949cc86e57811e5a814a58389a662389794a40fe967d86714b7de82943ad0047eba9e6111ca6d6156db4ac850aeaaae45498afe570061d487378012461a91'
